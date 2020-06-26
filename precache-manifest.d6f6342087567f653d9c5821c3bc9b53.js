fbaee544ab78c0649149ac8c843fece8fd4bfcb4
sumedha-prithyani <sumedhaprithyani24@gmail.com>
2020-06-26 21:12:10 +0530

    Add files via upload

---
+++ precache-manifest.d6f6342087567f653d9c5821c3bc9b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2393530e8235969b2121695352ac4d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20259c0f6386d603cc08",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "606c33895002c72f276d",
+    "url": "/static/css/main.8ce4de64.chunk.css"
+  },
+  {
+    "revision": "20259c0f6386d603cc08",
+    "url": "/static/js/2.6bd3656d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6bd3656d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606c33895002c72f276d",
+    "url": "/static/js/main.2614c641.chunk.js"
+  },
+  {
+    "revision": "ead0b681b71aa0a3a604",
+    "url": "/static/js/runtime-main.4035aaa9.js"
+  }
+]);
